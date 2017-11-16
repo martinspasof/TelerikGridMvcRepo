6f6672745a549274370a00eb37e1aaac097efeed
Martin Spasov <martinspasof@gmail.com>
2017-11-16 02:35:51 +0200

    Use Telerik Grid for Show Data of Top Companies

---
+++ TelerikGridMvc/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelerikGridMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            List<BigCompany> list = new List<BigCompany>();
+            using(CompaniesDbEntities dc = new CompaniesDbEntities())
+            {
+                list = dc.BigCompanies.OrderBy(o => o.Rank).ToList();
+            }
+            return View(list);
+        }
+
+    }
+}
